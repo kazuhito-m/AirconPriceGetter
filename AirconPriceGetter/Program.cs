e28f639534a0b7ae76050ebf295e65b6fe22ff69
kazuhito-m <kazuhito.sumpic@gmail.com>
2017-06-29 18:09:21 +0900

    最初のコミット。とりあえず、いろいろテスト書いた状態から。

---
+++ AirconPriceGetter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace AirconPriceGetter
+{
+    class Program
+    {
+
+        private const string URL_TEMPLATE = "http://kakaku.com/search_results/{0}/?sort=priceb";
+
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.ScrapingPlices(args[0], "./result.txt");
+        }
+
+        public void ScrapingPlices(string filePath, string outPath)
+        {
+            List<string> ids = LoadTextFile(filePath);
+
+            ids.Select(i => ToUrl(i))
+               .ToList()
+               .ForEach(i => Console.WriteLine(i));
+        }
+
+        protected internal List<string> LoadTextFile(string filePath)
+        {
+            StreamReader sr = new StreamReader(filePath, Encoding.GetEncoding("UTF-8"));
+            string text = sr.ReadToEnd();
+            string[] lines = text.Split('\n');
+            return new List<string>(lines);
+        }
+
+        protected internal string ToUrl(string id)
+        {
+            return String.Format(URL_TEMPLATE, id);
+        }
+
+
+
+    }
+}
---
+++ AirconPriceGetter/ProgramTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace AirconPriceGetter
+{
+    [TestFixture]
+    public class ProgramTest
+    {
+
+        [Test]
+        public void プログラム全体のスモーク()
+        {
+            string testWorkPath = TestContext.CurrentContext.TestDirectory;
+            string[] param = new string[] { testWorkPath + "../../../TestResources/id.txt" };
+
+            Program.Main(param);
+            Assert.AreEqual(true, true);
+        }
+
+        [Test]
+        public void 指定されたpathからテキストが読める()
+        {
+            string testWorkPath = TestContext.CurrentContext.TestDirectory;
+            Program sut = new Program();
+
+            List<string> actual = sut.LoadTextFile(testWorkPath + "../../../TestResources/id.txt");
+
+            Assert.That(actual[0], Is.EqualTo("SRK56ST2"));
+        }
+
+		[Test]
+		public void 指定した文字列をURLに変換出来る()
+		{
+			Program sut = new Program();
+
+            string actual = sut.ToUrl("SRK56ST2");
+
+			Assert.That(actual, Is.EqualTo("http://kakaku.com/search_results/SRK56ST2/?sort=priceb"));
+		}
+
+	}
+}
